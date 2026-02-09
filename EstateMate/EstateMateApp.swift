f6ba83ed6bf2a6eafd49c8bbda42f266a59e2339
Jiashu Li <452472803ljs@gmail.com>
2026-02-09 13:04:08 -0500

    Initial Commit

---
+++ EstateMate/EstateMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstateMateApp.swift
+//  EstateMate
+//
+//  Created by Jason Li on 2026-02-09.
+//
+
+import SwiftUI
+
+@main
+struct EstateMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
